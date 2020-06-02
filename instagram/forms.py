5050cd72fb9c4bb556d2cc9dfa5ef96dc7c98829
Ianadika44 <adika19ian@gmail.com>
2020-06-02 23:14:54 +0300

    create a form for our new post.

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from .models import Post
 
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
+    
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['profile']
+        widgets = {
+            'following': forms.CheckboxSelectMultiple(),
+        }
